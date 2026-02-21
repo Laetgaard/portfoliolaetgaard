881ae8b1b49fb8fc8e130470003dedaea2b10f5d
ChristofferLaet <chl001@edu.zealand.dk>
2026-02-21 20:11:11 +0100

    60% done

---
+++ forside.js
@@ -0,0 +1,45 @@
+const btn = document.querySelector('.burger');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+});
+
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+
+let index = 0;
+
+
+function showSlide(i) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[i].classList.add("active");
+}
+
+
+nextBtn.addEventListener("click", () => {
+
+    index++;
+
+    if (index >= slides.length) {
+        index = 0;
+    }
+
+    showSlide(index);
+});
+
+
+prevBtn.addEventListener("click", () => {
+
+    index--;
+
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+
+    showSlide(index);
+});
